6ca731e10cd93bc729d3b1840fbb333b09f20863
ofec <ofec.manor@gmail.com>
2013-11-26 14:33:06 +0200

    adding lab 2 files

---
+++ divisors.py
@@ -0,0 +1,6 @@
+n=int(raw_input("Number: "))
+def divisors(n):
+	for i in range(1,n+1):
+		if n%i==0:
+			print i
+divisors(n)
---
+++ palindrome.py
@@ -0,0 +1,3 @@
+word=raw_input("type word: ")
+for a in range(0.5*len(word)):
+	print a
